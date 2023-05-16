1506dfded4dbd13d6f5f29a1b2c7bdefd10552f7
Fernando Aguilar <feraguilar6985@gmail.com>
2023-05-16 21:32:18 +0000

    :sparkles: (feat):add enpoint using query params

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,27 +1,40 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  // Default endpoint
   @Get()
   getHello(): string {
     return 'Hello NestJS!';
   }
 
+  // New endpoint
   @Get('new')
   newEndpoint(): string {
     return 'My new endpoint';
   }
 
+  // New endpoint with params
   @Get('product/:productId')
   getProduct(@Param('productId') productId: string) {
     return `Product ${productId}`;
   }
 
+  // New endpoint with two params
   @Get('categories/:categoryId/products/:productId')
-  getCategory(@Param('categoryId') categoryId: string, @Param('productId') productId: string): string {
+  getCategory(
+    @Param('categoryId') categoryId: string,
+    @Param('productId') productId: string,
+  ): string {
     return `Product ${productId} and Category ${categoryId}`;
+  }
+
+  // New endpoint with query params
+  @Get('products')
+  getProducts(@Query('limit') limit = 100, @Query('offset') offset = 0) {
+    return `Products: limit => ${limit} offset => ${offset}`;
   }
 }
