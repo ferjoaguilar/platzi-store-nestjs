3d39a82f0f4254b93efa9855dd2374bfa19f8c7a
Fernando Aguilar <feraguilar6985@gmail.com>
2023-05-18 03:52:18 +0000

    :sparkles: (feat):add services

--- src/services/products/products.service.ts
+++ src/services/products/products.service.ts
@@ -1,4 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { Product } from './../../entities/product.entity';
 
 @Injectable()
-export class ProductsService {}
+export class ProductsService {
+  private products: Product[] = [
+    {
+      id: 1,
+      name: 'Product 1',
+      description: 'bla bla bla',
+      price: 122,
+      image: 'https://i.imgur.com/U4iGx1j.jpeg',
+      stock: 12,
+    },
+  ];
+
+  findAll() {
+    return this.products;
+  }
+
+  findOne(id: number) {
+    return this.products.find((item) => item.id === id);
+  }
+
+  create(payload: any) {
+    const newProduct = {
+      id: this.products.length + 1,
+      ...payload,
+    };
+    this.products.push(newProduct);
+    return newProduct;
+  }
+}
