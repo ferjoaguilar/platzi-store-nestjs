1801ebdd0eb91fdc5f905ac871536692539887bf
Fernando Aguilar <feraguilar6985@gmail.com>
2023-05-23 03:46:44 +0000

    ::sparkles (feat): add dto validation

--- src/dtos/products.dto.ts
+++ src/dtos/products.dto.ts
@@ -1,8 +1,32 @@
+import {
+  IsString,
+  IsNumber,
+  IsUrl,
+  IsNotEmpty,
+  IsPositive,
+} from 'class-validator';
+
 export class CreateProductDto {
+  @IsString()
+  @IsNotEmpty()
   readonly name: string;
+
+  @IsString()
+  @IsNotEmpty()
   readonly description: string;
-  readonly price: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  @IsPositive()
+  readonly price: number;
+
+  @IsNumber()
+  @IsNotEmpty()
+  @IsPositive()
   readonly stock: number;
+
+  @IsUrl()
+  @IsNotEmpty()
   readonly image: string;
 }
 
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // This will remove any property that is not in the DTO
+      forbidNonWhitelisted: true, // This will throw an error if a property is not in the DTO
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
